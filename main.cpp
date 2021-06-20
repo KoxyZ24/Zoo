8fd7aa995db74b566b91c63e697d3fb02f7b4072
Tom Hollingworth <74581101+THollingworth@users.noreply.github.com>
2021-06-20 23:50:15 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include "zoo.h"
-#include "aigle.h"
-#include "poule.h"
-#include "tigre.h"
+#include "shop.h"
+
 
 using namespace std;
 
@@ -10,21 +8,7 @@ int main()
 {
     Zoo zoo("Zoo de la Montagne");
 
-    zoo.addAnimal(new Tigre("Tigre 1"));
-    zoo.addAnimal(new Tigre("Tigre 2"));
-    zoo.addAnimal(new Poule("poule 1"));
-    zoo.addAnimal(new Poule("poule 2"));
-    zoo.addAnimal(new Poule("poule 2"));
-    zoo.addAnimal(new Poule("poule 3"));
-    zoo.addAnimal(new Poule("poule 4"));
-    zoo.addAnimal(new Poule("poule 5"));
-    zoo.addAnimal(new Poule("poule 6"));
-    zoo.addAnimal(new Poule("poule 7"));
-    zoo.addAnimal(new Poule("poule 8"));
-    zoo.addAnimal(new Poule("poule 9"));
-    zoo.addAnimal(new Poule("poule 10"));
-    zoo.addAnimal(new Aigle("aigle 1"));
-    zoo.addAnimal(new Aigle("aigle 2"));
+    shop.ActionShop();
 
     return 0;
 }
